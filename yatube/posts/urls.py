dbaba9774b09ea7c9f7c8edc7d5c901b1f227ae5
Roman Tatarenkov <flexomj@gmail.com>
2023-01-06 11:55:11 +0400

    connected img & CSS

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -7,6 +7,7 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('group/', views.group_posts, name='groups'),
+    path('group_list.html', views.group_posts, name='groups'),
 ]
 
     # path('group/<slug:slug>/', views.group_posts)
